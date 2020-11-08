48301c829a5c1ee3e80bc5eac6dd4d4f173ebefa
lamba <lamba@Dmitriys-MacBook-Air.local>
2020-11-08 15:49:49 +0300

    Changing at the datebase

--- readme.sql
+++ readme.sql
@@ -1,250 +1,5 @@
--- MySQL dump 10.13  Distrib 8.0.19, for macos10.15 (x86_64)
---
--- Host: 127.0.0.1    Database: readme
--- ------------------------------------------------------
--- Server version	5.7.30
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `comment`
---
-
-DROP TABLE IF EXISTS `comment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `crate_time` datetime NOT NULL,
-  `content` text NOT NULL,
-  `post_creator_id` int(11) NOT NULL,
-  `post_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `comment_post_key_idx` (`post_creator_id`),
-  KEY `comment_post_key_idx1` (`post_id`),
-  CONSTRAINT `comment_post_creator_key` FOREIGN KEY (`post_creator_id`) REFERENCES `post` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `comment_post_key` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `comment`
---
-
-LOCK TABLES `comment` WRITE;
-/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `content_type`
---
-
-DROP TABLE IF EXISTS `content_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `content_type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `class_icon` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `content_type`
---
-
-LOCK TABLES `content_type` WRITE;
-/*!40000 ALTER TABLE `content_type` DISABLE KEYS */;
-INSERT INTO `content_type` VALUES (1,'Текст','text'),(2,'Цитата','quote'),(3,'Картинка','photo'),(4,'Видео','video'),(5,'Ссылка','link');
-/*!40000 ALTER TABLE `content_type` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `hashtag`
---
-
-DROP TABLE IF EXISTS `hashtag`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `hashtag` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `hashtag`
---
-
-LOCK TABLES `hashtag` WRITE;
-/*!40000 ALTER TABLE `hashtag` DISABLE KEYS */;
-/*!40000 ALTER TABLE `hashtag` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `like`
---
-
-DROP TABLE IF EXISTS `like`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `like` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `post_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `like_user_key_idx` (`user_id`),
-  KEY `like_post_key_idx` (`post_id`),
-  CONSTRAINT `like_post_key` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `like_user_key` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `like`
---
-
-LOCK TABLES `like` WRITE;
-/*!40000 ALTER TABLE `like` DISABLE KEYS */;
-/*!40000 ALTER TABLE `like` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `message`
---
-
-DROP TABLE IF EXISTS `message`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `message` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `create_time` datetime NOT NULL,
-  `content` text NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `message_user_key_idx` (`user_id`),
-  CONSTRAINT `message_user_key` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `message`
---
-
-LOCK TABLES `message` WRITE;
-/*!40000 ALTER TABLE `message` DISABLE KEYS */;
-/*!40000 ALTER TABLE `message` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `post`
---
-
-DROP TABLE IF EXISTS `post`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `post` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `create_time` datetime NOT NULL,
-  `header` varchar(100) DEFAULT NULL,
-  `content` varchar(255) DEFAULT NULL,
-  `quote_author` varchar(100) DEFAULT NULL,
-  `picture` varchar(100) DEFAULT NULL,
-  `video` varchar(100) DEFAULT NULL,
-  `link` varchar(100) DEFAULT NULL,
-  `count_views` int(11) unsigned NOT NULL,
-  `content_type_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `post_content_type_key_idx` (`content_type_id`),
-  KEY `post_user_key_idx` (`user_id`),
-  CONSTRAINT `post_content_type_key` FOREIGN KEY (`content_type_id`) REFERENCES `content_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `post_user_key` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `post`
---
-
-LOCK TABLES `post` WRITE;
-/*!40000 ALTER TABLE `post` DISABLE KEYS */;
-/*!40000 ALTER TABLE `post` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `post_hashtag`
---
-
-DROP TABLE IF EXISTS `post_hashtag`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `post_hashtag` (
-  `id` int(10) unsigned NOT NULL,
-  `post_id` int(11) NOT NULL,
-  `hashtag_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `post_hashtag_post_key_idx` (`post_id`),
-  KEY `sdf_idx` (`hashtag_id`),
-  CONSTRAINT `post_hashtag_hashtag_key` FOREIGN KEY (`hashtag_id`) REFERENCES `hashtag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `post_hashtag_post_key` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `post_hashtag`
---
-
-LOCK TABLES `post_hashtag` WRITE;
-/*!40000 ALTER TABLE `post_hashtag` DISABLE KEYS */;
-/*!40000 ALTER TABLE `post_hashtag` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `subscribe`
---
-
-DROP TABLE IF EXISTS `subscribe`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `subscribe` (
-  `id` int(11) NOT NULL,
-  `author` int(11) NOT NULL,
-  `subscribed` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `subscribe`
---
-
-LOCK TABLES `subscribe` WRITE;
-/*!40000 ALTER TABLE `subscribe` DISABLE KEYS */;
-/*!40000 ALTER TABLE `subscribe` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `reg_date` datetime NOT NULL,
   `email` varchar(100) NOT NULL,
   `login` varchar(100) NOT NULL,
@@ -253,24 +8,91 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `user`
---
+CREATE TABLE `post` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `create_time` datetime NOT NULL,
+  `header` varchar(100) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `quote_author` varchar(100) DEFAULT NULL,
+  `picture` varchar(100) DEFAULT NULL,
+  `video` varchar(100) DEFAULT NULL,
+  `link` varchar(100) DEFAULT NULL,
+  `count_views` int unsigned NOT NULL,
+  `content_type_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `post_content_type_key_idx` (`content_type_id`),
+  KEY `post_user_key_idx` (`user_id`),
+  CONSTRAINT `post_content_type_key` FOREIGN KEY (`content_type_id`) REFERENCES `content_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `post_user_key` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+CREATE TABLE `content_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `class_icon` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+INSERT INTO `content_type` VALUES (1,'Текст','text'),(2,'Цитата','quote'),(3,'Картинка','photo'),(4,'Видео','video'),(5,'Ссылка','link');
 
--- Dump completed on 2020-11-03 19:42:58
+CREATE TABLE `hashtag` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `post_hashtag` (
+  `id` int unsigned NOT NULL,
+  `post_id` int NOT NULL,
+  `hashtag_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `post_hashtag_post_key_idx` (`post_id`),
+  KEY `sdf_idx` (`hashtag_id`),
+  CONSTRAINT `post_hashtag_hashtag_key` FOREIGN KEY (`hashtag_id`) REFERENCES `hashtag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `post_hashtag_post_key` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `comment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `crate_time` datetime NOT NULL,
+  `content` text NOT NULL,
+  `post_creator_id` int NOT NULL,
+  `post_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comment_post_creator_key_idx` (`post_creator_id`),
+  KEY `comment_post_key_idx` (`post_id`),
+  CONSTRAINT `comment_post_creator_key` FOREIGN KEY (`post_creator_id`) REFERENCES `post` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `comment_post_key` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `like` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `post_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `like_user_key_idx` (`user_id`),
+  KEY `like_post_key_idx` (`post_id`),
+  CONSTRAINT `like_post_key` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `like_user_key` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `subscribe` (
+  `id` int NOT NULL,
+  `author` int NOT NULL,
+  `subscribed` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `message` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `create_time` datetime NOT NULL,
+  `content` text NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `message_user_key_idx` (`user_id`),
+  CONSTRAINT `message_user_key` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
