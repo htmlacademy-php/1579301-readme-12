257bbcc5a84a93ddb3f31799e1433af243f3a358
lamba <lamba@Dmitriys-MacBook-Air.local>
2020-11-16 15:49:58 +0300

    sql queries

---
+++ queries.sql
@@ -0,0 +1,9 @@
+INSERT INTO `post` VALUES (1, generate_random_date(0), 'Цитата', 'Мы в жизни любим только раз, а после ищем лишь похожих.Мы в жизни любим только раз, а после ищем лишь похожих.', 'Неизвестный Автор', NULL, NULL, NULL, 0, 2, 1, 0, 1);
+INSERT INTO `post` VALUES (2, generate_random_date(1), 'Игра престолов', 'Не могу дождаться начала финального сезона своего любимого сериала!', NULL, NULL, NULL, NULL, 0, 1, 2, 0, 2;
+INSERT INTO `post` VALUES (3, generate_random_date(0), 'Наконец, обработал фотки!', NULL, NULL, 'rock-medium.jpg', NULL, NULL, 0, 3, 3, 0, 3);
+INSERT INTO `post` VALUES (4, generate_random_date(0), 'Моя мечта', NULL, NULL, 'coast-medium.jpg', NULL, NULL, 0, 3, 1, 0, 1);
+INSERT INTO `post` VALUES (5, generate_random_date(0), 'Лучшие курсы', NULL, NULL, NULL, NULL, 'www.htmlacademy.ru', 0, 5, 2, 0, 2);
+
+INSERT INTO `user` VALUES (1, '2020-07-25 18:01:02', 'larisa@mail.com', 'larisa', 1111, 'userpic-larisa-small.jpg');
+INSERT INTO `user` VALUES (2, '2019-02-15 12:15:24', 'vladik@mail.com', 'vladik', 2222, 'userpic.jpg');
+INSERT INTO `user` VALUES (3, '2018-05-02 09:15:00', 'victor@mail.com', 'victor', 3333, 'userpic-mark.jpg');
