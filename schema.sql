0a4f5e7503d5f8c8e4110b55bd678af03e24850c
lamba <lamba@Dmitriys-MacBook-Air.local>
2020-11-16 15:44:43 +0300

    datebase

--- schema.sql
+++ schema.sql
@@ -6,26 +6,30 @@ CREATE TABLE `user` (
   `password` varchar(100) NOT NULL,
   `avatar` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
+  UNIQUE KEY `user_email_udx` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `post` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `create_time` datetime NOT NULL,
   `header` varchar(100) DEFAULT NULL,
-  `content` varchar(255) DEFAULT NULL,
+  `content` text DEFAULT NULL,
   `quote_author` varchar(100) DEFAULT NULL,
   `picture` varchar(100) DEFAULT NULL,
   `video` varchar(100) DEFAULT NULL,
   `link` varchar(100) DEFAULT NULL,
-  `count_views` int UNSIGNED NOT NULL,
+  `count_views` int UNSIGNED DEFAULT 0,
   `content_type_id` int NOT NULL,
   `user_id` int NOT NULL,
+  `is_repost` tinyint(1) DEFAULT 0,
+  `original_user_id` int NULL,
   PRIMARY KEY (`id`),
-  KEY `post_content_type_fk_idx` (`content_type_id`),
-  KEY `post_user_fk_idx` (`user_id`),
-  CONSTRAINT `post_content_type_fk` FOREIGN KEY (`content_type_id`) REFERENCES `content_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `post_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `post_content_type_id_idx` (`content_type_id`),
+  KEY `post_user_id_idx` (`user_id`),
+  KEY `post_original_user_id` (`original_user_id`),
+  CONSTRAINT `post_content_type_id_fk` FOREIGN KEY (`content_type_id`) REFERENCES `content_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `post_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `post_original_user_id_fk` FOREIGN KEY (`original_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `content_type` (
@@ -48,10 +52,10 @@ CREATE TABLE `post_hashtag` (
   `post_id` int NOT NULL,
   `hashtag_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `post_hashtag_post_fk_idx` (`post_id`),
-  KEY `post_hashtag_hashtag_fk_idx` (`hashtag_id`),
-  CONSTRAINT `post_hashtag_hashtag_fk` FOREIGN KEY (`hashtag_id`) REFERENCES `hashtag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `post_hashtag_post_fk` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `post_hashtag_post_id_idx` (`post_id`),
+  KEY `post_hashtag_hashtag_id_idx` (`hashtag_id`),
+  CONSTRAINT `post_hashtag_hashtag_id_fk` FOREIGN KEY (`hashtag_id`) REFERENCES `hashtag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `post_hashtag_post_id_fk` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `comment` (
@@ -61,10 +65,10 @@ CREATE TABLE `comment` (
   `post_creator_id` int NOT NULL,
   `post_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `comment_post_creator_fk_idx` (`post_creator_id`),
-  KEY `comment_post_fk_idx` (`post_id`),
-  CONSTRAINT `comment_post_creator_fk` FOREIGN KEY (`post_creator_id`) REFERENCES `post` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `comment_post_fk` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `comment_post_creator_id_idx` (`post_creator_id`),
+  KEY `comment_post_id_idx` (`post_id`),
+  CONSTRAINT `comment_post_creator_id_fk` FOREIGN KEY (`post_creator_id`) REFERENCES `post` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `comment_post_id_fk` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `like` (
@@ -72,10 +76,10 @@ CREATE TABLE `like` (
   `user_id` int NOT NULL,
   `post_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `like_user_fk_idx` (`user_id`),
-  KEY `like_post_fk_idx` (`post_id`),
-  CONSTRAINT `like_post_fk` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `like_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `like_user_id_idx` (`user_id`),
+  KEY `like_post_id_idx` (`post_id`),
+  CONSTRAINT `like_post_id_fk` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `like_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `subscribe` (
@@ -83,10 +87,10 @@ CREATE TABLE `subscribe` (
   `author_id` int NOT NULL,
   `subscriber_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `subscribe_author_id_user_fk_idx` (`author_id`),
-  KEY `subscribe_subscriber_id_user_fk_idx` (`subscriber_id`),
-  CONSTRAINT `subscribe_author_id_user_fk` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `subscribe_subscriber_id_user_fk` FOREIGN KEY (`subscriber_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `subscribe_author_id_idx` (`author_id`),
+  KEY `subscribe_subscriber_id_idx` (`subscriber_id`),
+  CONSTRAINT `subscribe_author_id_fk` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `subscribe_subscriber_id_fk` FOREIGN KEY (`subscriber_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `message` (
@@ -97,10 +101,10 @@ CREATE TABLE `message` (
   `recipient_id` int NOT NULL,
   `is_read` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `message_sender_id_user_fk_idx` (`sender_id`),
-  KEY `message_recipient_id_user_idx` (`recipient_id`),
-  CONSTRAINT `message_sender_id_user_fk` FOREIGN KEY (`sender_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `message_recipient_id_user_fk` FOREIGN KEY (`recipient_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `message_sender_id_idx` (`sender_id`),
+  KEY `message_recipient_id_idx` (`recipient_id`),
+  CONSTRAINT `message_sender_id_fk` FOREIGN KEY (`sender_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `message_recipient_id_fk` FOREIGN KEY (`recipient_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
