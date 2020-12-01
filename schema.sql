e40f6888da7e2987273826629e208c836fc8de8f
lamba <lamba@Dmitriys-MacBook-Air.local>
2020-12-01 16:28:38 +0300

    schema

--- schema.sql
+++ schema.sql
@@ -9,6 +9,15 @@ CREATE TABLE `user` (
   UNIQUE KEY `user_email_udx` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `content_type` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `class_icon` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+INSERT INTO `content_type` VALUES (1,'Текст','text'),(2,'Цитата','quote'),(3,'Картинка','photo'),(4,'Видео','video'),(5,'Ссылка','link');
+
 CREATE TABLE `post` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `create_time` datetime NOT NULL,
@@ -19,10 +28,10 @@ CREATE TABLE `post` (
   `video` varchar(100) DEFAULT NULL,
   `link` varchar(100) DEFAULT NULL,
   `count_views` int UNSIGNED DEFAULT 0,
-  `content_type_id` int NOT NULL,
-  `user_id` int NOT NULL,
+  `content_type_id` int UNSIGNED NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
   `is_repost` tinyint(1) DEFAULT 0,
-  `original_user_id` int NULL,
+  `original_user_id` int UNSIGNED NULL,
   PRIMARY KEY (`id`),
   KEY `post_content_type_id_idx` (`content_type_id`),
   KEY `post_user_id_idx` (`user_id`),
@@ -32,15 +41,6 @@ CREATE TABLE `post` (
   CONSTRAINT `post_original_user_id_fk` FOREIGN KEY (`original_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `content_type` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `class_icon` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `content_type` VALUES (1,'Текст','text'),(2,'Цитата','quote'),(3,'Картинка','photo'),(4,'Видео','video'),(5,'Ссылка','link');
-
 CREATE TABLE `hashtag` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -49,8 +49,8 @@ CREATE TABLE `hashtag` (
 
 CREATE TABLE `post_hashtag` (
   `id` int UNSIGNED NOT NULL,
-  `post_id` int NOT NULL,
-  `hashtag_id` int NOT NULL,
+  `post_id` int UNSIGNED NOT NULL,
+  `hashtag_id` int UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   KEY `post_hashtag_post_id_idx` (`post_id`),
   KEY `post_hashtag_hashtag_id_idx` (`hashtag_id`),
@@ -62,8 +62,8 @@ CREATE TABLE `comment` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `create_time` datetime NOT NULL,
   `content` text NOT NULL,
-  `post_creator_id` int NOT NULL,
-  `post_id` int NOT NULL,
+  `post_creator_id` int UNSIGNED NOT NULL,
+  `post_id` int UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   KEY `comment_post_creator_id_idx` (`post_creator_id`),
   KEY `comment_post_id_idx` (`post_id`),
@@ -73,8 +73,8 @@ CREATE TABLE `comment` (
 
 CREATE TABLE `like` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `post_id` int NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `post_id` int UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   KEY `like_user_id_idx` (`user_id`),
   KEY `like_post_id_idx` (`post_id`),
@@ -84,8 +84,8 @@ CREATE TABLE `like` (
 
 CREATE TABLE `subscribe` (
   `id` int UNSIGNED NOT NULL,
-  `author_id` int NOT NULL,
-  `subscriber_id` int NOT NULL,
+  `author_id` int UNSIGNED NOT NULL,
+  `subscriber_id` int UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   KEY `subscribe_author_id_idx` (`author_id`),
   KEY `subscribe_subscriber_id_idx` (`subscriber_id`),
@@ -97,8 +97,8 @@ CREATE TABLE `message` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `create_time` datetime NOT NULL,
   `content` text NOT NULL,
-  `sender_id` int NOT NULL,
-  `recipient_id` int NOT NULL,
+  `sender_id` int UNSIGNED NOT NULL,
+  `recipient_id` int UNSIGNED NOT NULL,
   `is_read` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `message_sender_id_idx` (`sender_id`),
