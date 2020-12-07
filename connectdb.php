b4b5e4ecfd1635f489ceba925b2b6990aee0ae67
lamba <lamba@Dmitriys-MacBook-Air.local>
2020-12-07 22:29:30 +0300

    Connecting to datebase

--- connectdb.php
+++ connectdb.php
@@ -1,1 +1,18 @@
 <?php
+
+$host = 'localhost';
+$login = 'root';
+$password = 'root';
+$bdname = 'readme';
+
+$connect = mysqli_connect($host, $login, $password, $bdname);
+
+mysqli_set_charset($connect, "utf8");
+
+if (!$connect) {
+    print("Ошибка подключения: " . mysqli_connect_error());
+}
+else {
+    print("Соединение установлено");
+
+}
